e7b73e30e9fd3a8708f55ecb86992546209ac7ca
VEGABIG\Rusll <rusllann97@gmail.com>
2023-11-12 22:55:39 +0500

    Отключено использование PC13 библиотекой USB. Вроде работает.

--- stm_lib/src/hw_config.c
+++ stm_lib/src/hw_config.c
@@ -72,11 +72,11 @@ void Set_System(void)
 
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
 
-  /* Configure USB pull-up pin */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_OD;
-  GPIO_Init(GPIOC, &GPIO_InitStructure);
+//  /* Configure USB pull-up pin */
+//  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
+//  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+//  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_OD;
+//  GPIO_Init(GPIOC, &GPIO_InitStructure);
 
 
   /*Set PA11,12 as IN - USB_DM,DP*/

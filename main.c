408c9d306d3ebaa5cf612380f6ed35500bd2e6f8
VEGALITTLE\Rusll <rusllann97@gmail.com>
2023-11-14 23:05:05 +0500

    Добавлена первичная инциализация таймера для ШИМ. Пока просто чтобы перенести код

--- main.c
+++ main.c
@@ -12,14 +12,20 @@
 #include "usb_pwr.h"
 #include "stm32f10x_i2c.h"
 
+#include "stm32f10x_tim.h"
+
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
 
 #include "bme280.h"
 
+
 ADC_InitTypeDef ADC_InitStructure;
 GPIO_InitTypeDef InitStruct;
+
+TIM_TimeBaseInitTypeDef timer1;
+TIM_OCInitTypeDef timer1Pwm;
 
 uint8_t res = 0;
 uint8_t i = 0;
@@ -54,7 +60,7 @@ int usb_printf(char *format, ...)
 	return 0;
 }
 
-void TIM2_IRQHandler()
+void TIM1_IRQHandler()
 {
 	//res = (i-63)*(i-63)/17;
 	res = 50*(1 - exp(-0.02*i));
@@ -80,6 +86,31 @@ int main(void)
     __enable_irq();
 
     bme280_init();
+
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM1, ENABLE);
+
+    GPIO_InitTypeDef gpioStruct;
+    GPIO_StructInit(&gpioStruct);
+    gpioStruct.GPIO_Mode = GPIO_Mode_AF_PP;
+    gpioStruct.GPIO_Pin = GPIO_Pin_7;
+    gpioStruct.GPIO_Speed = GPIO_Speed_2MHz;
+	GPIO_Init(GPIOB, &gpioStruct);
+
+	TIM_TimeBaseStructInit(&timer1);
+	timer1.TIM_Prescaler = 720;
+	timer1.TIM_Period = 250;
+	TIM_TimeBaseInit(TIM1, &timer1);
+
+	TIM_OCStructInit(&timer1Pwm);
+	timer1Pwm.TIM_Pulse = 50;
+	timer1Pwm.TIM_OCMode = TIM_OCMode_PWM1;
+	timer1Pwm.TIM_OutputState = TIM_OutputState_Enable;
+	TIM_OC1Init(TIM1, &timer1Pwm);
+
+	TIM_ITConfig(TIM1, TIM_IT_Update, ENABLE);
+	TIM_Cmd(TIM1, ENABLE);
+	NVIC_EnableIRQ(TIM1_IRQn);
+
 
     while (1)
     {

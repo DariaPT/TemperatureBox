b7d91a8d1198054e48ea3e2e84fbd05300a770e4
VEGABIG\Rusll <rusllann97@gmail.com>
2023-11-17 00:08:47 +0500

    Добавлено корректное управление ШИМ

--- main.c
+++ main.c
@@ -24,8 +24,11 @@
 ADC_InitTypeDef ADC_InitStructure;
 GPIO_InitTypeDef InitStruct;
 
-TIM_TimeBaseInitTypeDef timer1;
-TIM_OCInitTypeDef timer1Pwm;
+uint16_t previousState;
+GPIO_InitTypeDef port;
+TIM_TimeBaseInitTypeDef timer;
+TIM_OCInitTypeDef timerPWM;
+uint16_t buttonPreviousState;
 
 uint8_t res = 0;
 uint8_t i = 0;
@@ -60,16 +63,6 @@ int usb_printf(char *format, ...)
 	return 0;
 }
 
-void TIM1_IRQHandler()
-{
-	//res = (i-63)*(i-63)/17;
-	res = 50*(1 - exp(-0.02*i));
-	i++;
-
-
-	USB_Send_Data(res);
-	TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
-}
 
 void USB_Init_Function()
 {
@@ -77,6 +70,11 @@ void USB_Init_Function()
     Set_USBClock();
     USB_Interrupts_Config();
     USB_Init();
+}
+
+void TIM3_IRQHandler()
+{
+	TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
 }
 
 int main(void)
@@ -87,37 +85,56 @@ int main(void)
 
     bme280_init();
 
-    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM1, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
 
-    GPIO_InitTypeDef gpioStruct;
-    GPIO_StructInit(&gpioStruct);
-    gpioStruct.GPIO_Mode = GPIO_Mode_AF_PP;
-    gpioStruct.GPIO_Pin = GPIO_Pin_7;
-    gpioStruct.GPIO_Speed = GPIO_Speed_2MHz;
-	GPIO_Init(GPIOB, &gpioStruct);
+    GPIO_StructInit(&port);
+    port.GPIO_Mode = GPIO_Mode_AF_PP;
+    port.GPIO_Pin = GPIO_Pin_1;
+    port.GPIO_Speed = GPIO_Speed_2MHz;
+    GPIO_Init(GPIOB, &port);
 
-	TIM_TimeBaseStructInit(&timer1);
-	timer1.TIM_Prescaler = 720;
-	timer1.TIM_Period = 250;
-	TIM_TimeBaseInit(TIM1, &timer1);
+    TIM_TimeBaseStructInit(&timer);
+    timer.TIM_Prescaler = 720; // 1 tick per 0.01 ms
+    timer.TIM_Period = 255;
+    TIM_TimeBaseInit(TIM3, &timer);
 
-	TIM_OCStructInit(&timer1Pwm);
-	timer1Pwm.TIM_Pulse = 50;
-	timer1Pwm.TIM_OCMode = TIM_OCMode_PWM1;
-	timer1Pwm.TIM_OutputState = TIM_OutputState_Enable;
-	TIM_OC1Init(TIM1, &timer1Pwm);
+    TIM_OCStructInit(&timerPWM);
+    timerPWM.TIM_Pulse = 50; // 0.5 ms
+    timerPWM.TIM_OCMode = TIM_OCMode_PWM1;
+    timerPWM.TIM_OutputState = TIM_OutputState_Enable;
+    TIM_OC4Init(TIM3, &timerPWM);
 
-	TIM_ITConfig(TIM1, TIM_IT_Update, ENABLE);
-	TIM_Cmd(TIM1, ENABLE);
-	NVIC_EnableIRQ(TIM1_IRQn);
+    TIM_ITConfig(TIM3, TIM_IT_Update, ENABLE);
+    TIM_Cmd(TIM3, ENABLE);
+    NVIC_EnableIRQ(TIM3_IRQn);
 
 
     while (1)
     {
     	int8_t currentTemperatureInCelcius = bme280_get_temp_in_celcius();
 
-		USB_Send_Data(currentTemperatureInCelcius);
 
-    	_delay_ms(10000000);
+    	static uint8_t counter = 0;
+        TIM3->CCR4 = counter;
+        if(++counter >= 255)
+        {
+        	counter = 0;
+        }
+
+		for (int var = 0; var < 256; ++var)
+		{
+			TIM3->CCR4 = var;
+			_delay_ms(10000);
+		}
+
+		for (int var = 255; var >= 0; var--)
+		{
+			TIM3->CCR4 = var;
+			_delay_ms(10000);
+		}
+
+    	USB_Send_Data(currentTemperatureInCelcius);
+
     }
 }

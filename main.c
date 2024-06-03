189c1d76efe33f0523554f5ef7bea72091405e98
VEGABIG\Rusll <rusllann97@yandex.ru>
2024-06-04 00:20:22 +0500

    Добавил основную функциональность старта измерений

--- main.c
+++ main.c
@@ -20,6 +20,8 @@
 #include "FreeRTOS/Include/queue.h"
 #include "FreeRTOS/Include/task.h"
 
+#include "FreeRTOS/Include/semphr.h"
+
 #include "custom_pwm.h"
 
 #define USE_DHT22_SENSOR 1
@@ -30,6 +32,8 @@
 #else
 #include "bme280.h"
 #endif
+
+#define PUMP_WORK_TIME_MS 15000
 
 #define START_MEASUREMENT_INPUT_PORT_RCC RCC_APB2Periph_GPIOA
 #define START_MEASUREMENT_INPUT_PORT GPIOA
@@ -42,12 +46,24 @@
 #define PUMP_OUTPUT_PIN GPIO_Pin_2
 
 #define OUTPUT_SIGNAL_OUTPUT_PORT GPIOA
-#define OUTPUT_SIGNAL_OUTPUT_PIN GPIO_Pin_2
+#define OUTPUT_SIGNAL_OUTPUT_PIN GPIO_Pin_3
 
+#define PUMP_START() GPIO_SetBits(PUMP_OUTPUT_PORT, PUMP_OUTPUT_PIN)
+#define PUMP_STOP() GPIO_ResetBits(PUMP_OUTPUT_PORT, PUMP_OUTPUT_PIN)
+
+#define OPEN_VALVES() GPIO_SetBits(VALVES_OUTPUT_PORT, VALVES_OUTPUT_PIN)
+#define CLOSE_VALVES() GPIO_ResetBits(VALVES_OUTPUT_PORT, VALVES_OUTPUT_PIN)
+
+#define SET_OUTPUT_SIGNAL() GPIO_SetBits(OUTPUT_SIGNAL_OUTPUT_PORT, OUTPUT_SIGNAL_OUTPUT_PIN)
+#define RESET_OUTPUT_SIGNAL() GPIO_ResetBits(OUTPUT_SIGNAL_OUTPUT_PORT, OUTPUT_SIGNAL_OUTPUT_PIN)
 
 ADC_InitTypeDef ADC_InitStructure;
 GPIO_InitTypeDef InitStruct;
 xQueueHandle temperatureQueue;
+
+xSemaphoreHandle readyToMeasureSemaphoreHandle;
+
+volatile bool isReadyToHeat = false;
 
 void _delay_ms(uint32_t t)
 {
@@ -73,13 +89,25 @@ void USB_Init_Function()
 
 void MainTask(void *pvParameters)
 {
-
+	bool isMeasurementStarted = false;
+	isReadyToHeat = false;
 	while(1)
 	{
-		if(GPIO_ReadInputDataBit(START_MEASUREMENT_INPUT_PORT, START_MEASUREMENT_INPUT_PIN) == 1)
+		if(!isReadyToHeat && GPIO_ReadInputDataBit(START_MEASUREMENT_INPUT_PORT, START_MEASUREMENT_INPUT_PIN) == 1)
 		{
 			// Запустить измерение
 			send_bytes_array_to_usb("READY TO MEASURE\r\n", sizeof("READY TO MEASURE"));
+
+			OPEN_VALVES();
+			PUMP_START();
+
+			vTaskDelay(PUMP_WORK_TIME_MS);
+
+			CLOSE_VALVES();
+			PUMP_STOP();
+
+			isReadyToHeat = true;
+			//xSemaphoreGive(readyToMeasureSemaphoreHandle);
 		}
 		vTaskDelay(100);
 	}
@@ -96,27 +124,32 @@ void TaskSensorPoller(void *pvParameters)
 
 	while(1)
 	{
+		if(isReadyToHeat)
+		{
+			//xSemaphoreTake(readyToMeasureSemaphoreHandle, portMAX_DELAY);
+
 #if USE_DHT11_SENSOR
-		DHT11Read(&Rh,&RhDec,&Temp,&TempDec,&ChkSum);
-		double currentTemperatureInCelcius = (double)Temp;
+			DHT11Read(&Rh,&RhDec,&Temp,&TempDec,&ChkSum);
+			double currentTemperatureInCelcius = (double)Temp;
 #elif USE_DHT22_SENSOR
 
-		DHT22_Start();
-		Presence = DHT22_Check_Response();
-		Rh_byte1 = DHT22_Read();
-		Rh_byte2 = DHT22_Read();
-		Temp_byte1 = DHT22_Read();
-		Temp_byte2 = DHT22_Read();
+			DHT22_Start();
+			Presence = DHT22_Check_Response();
+			Rh_byte1 = DHT22_Read();
+			Rh_byte2 = DHT22_Read();
+			Temp_byte1 = DHT22_Read();
+			Temp_byte2 = DHT22_Read();
 
-		temperatureU16 = (Temp_byte1 << 8) | Temp_byte2;
+			temperatureU16 = (Temp_byte1 << 8) | Temp_byte2;
 
-		double currentTemperatureInCelcius = temperatureU16 / 10.0;
+			double currentTemperatureInCelcius = temperatureU16 / 10.0;
 #else
-		double currentTemperatureInCelcius = bmp280_get_float_temp();
+			double currentTemperatureInCelcius = bmp280_get_float_temp();
 #endif
 
 
-		xQueueSendToBack(temperatureQueue, (void *)&currentTemperatureInCelcius, 0);
+			xQueueSendToBack(temperatureQueue, (void *)&currentTemperatureInCelcius, 0);
+		}
 
 		vTaskDelay(100);
 	}
@@ -131,30 +164,51 @@ void PidRegulator(void *pvParameters)
 	double I_prev = 0;
 	double prevError = 0;
 
+
 	while(1)
 	{
 		double newTemperatureInCelcius = 0;
 		xQueueReceive(temperatureQueue, &newTemperatureInCelcius, portMAX_DELAY );
-		double currentError = T_st - newTemperatureInCelcius;
-		double P_part = Kp * currentError;
-		double I_part = I_prev + Ki * currentError;
-		double D_part = Kd * (currentError - prevError);
-		double newPwmValue = P_part + I_part + D_part;
-		if (newPwmValue < 0) newPwmValue = 0;
-		if (newPwmValue > 200) newPwmValue = 199;
 
-		CUSTOM_PWM_SET_NEW_VALUE((uint16_t)newPwmValue);
+		if(isReadyToHeat)
+		{
+			double currentError = T_st - newTemperatureInCelcius;
+			double P_part = Kp * currentError;
+			double I_part = I_prev + Ki * currentError;
+			double D_part = Kd * (currentError - prevError);
+			double newPwmValue = P_part + I_part + D_part;
 
-		prevError = currentError;
-		I_prev = I_part;
-		uint16_t currentTemperatureInCelciusX100 = newTemperatureInCelcius * 100;
-		send_bytes_array_to_usb((uint8_t*)&currentTemperatureInCelciusX100, 2);
+			if (newPwmValue < 0) newPwmValue = 0;
+			if (newPwmValue > 200) newPwmValue = 199;
+
+			CUSTOM_PWM_SET_NEW_VALUE((uint16_t)newPwmValue);
+
+			prevError = currentError;
+			I_prev = I_part;
+			uint16_t currentTemperatureInCelciusX100 = newTemperatureInCelcius * 100;
+			send_bytes_array_to_usb((uint8_t*)&currentTemperatureInCelciusX100, 2);
+
+			if(newTemperatureInCelcius >= 50)
+			{
+				CUSTOM_PWM_SET_NEW_VALUE(0); // Отключаем нагрев.
+
+				SET_OUTPUT_SIGNAL();
+
+				vTaskDelay(30000);
+
+				RESET_OUTPUT_SIGNAL();
+
+				isReadyToHeat = false;
+
+			}
+		}
 	}
 }
 
 void init_gpio()
 {
 	RCC_AHBPeriphClockCmd(START_MEASUREMENT_INPUT_PORT_RCC, ENABLE);
+	//RCC_AHBPeriphClockCmd(OUTPUT_SIGNAL_OUTPUT_RCC, ENABLE);
 
 	GPIO_InitTypeDef gpioStruct;
 	// Входной сигнал
@@ -184,6 +238,8 @@ void init_gpio()
 	gpioStruct.GPIO_Speed = GPIO_Speed_50MHz;
 
 	GPIO_Init(OUTPUT_SIGNAL_OUTPUT_PORT, &gpioStruct);
+
+	GPIO_Write(GPIOA, 0);
 }
 
 int main(void)
@@ -197,10 +253,13 @@ int main(void)
 	bmp280_init();
 #endif
 
+
     custom_pwm_init();
     temperatureQueue = xQueueCreate(1, sizeof(double));
 
     init_gpio();
+
+    vSemaphoreCreateBinary(readyToMeasureSemaphoreHandle);
 
     xTaskCreate(
     		MainTask,
